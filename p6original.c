f0793e0385c757e70ae6efaa3266381ee542141b
Fariha12t <96039101+Fariha12t@users.noreply.github.com>
2022-03-24 10:26:25 +0000

    count

--- p6original.c
+++ p6original.c
@@ -3,32 +3,57 @@
 void input_string(char *a)
 {
   printf("enter the string\n");
-  scanf("%s", a);
+  fgets(a,200,stdin);
 }
 int count_words(char *string)
 {
-  char a[20];
-  int i,count=0,sp=0;
-  for(i=0;a[i]!=0;i++)
+  int count=0;
+  for(int i=0;string[i]!='\0';i++)
     {
-      count++;
-        if(a[i]==' ')
-          sp++;
-      
+     if(string[i]==' ')
+     {
+       count=count+1;
+     }
     }
-   return count;
+  return count;
     
 }
+/*int count_words(char *string)
+{
+  char a[20];
+  int i=0,count=0, word=0;
+  do
+  
+    {
+      if(a[i]==' ' || a[i]=='\t' || a[i]=='\0')
+      {
+        if(word)
+        {
+          word=0;
+          count++;
+        }
+         else
+      {
+        word=1;
+      }
+      }
+      }
+      while(a[i++]);
+        
+   return count;
+    
+}*/
 void output(char *string, int no_words)
 {
+  
   printf("number of words %d",no_words+1);
 }
 int main()
 {
   char a[20];
   input_string(a);
-  int n;
-  n=count_words(a);
-  output(a,n);
+  int no_words;
+  no_words=count_words(a);
+  output(a,no_words);
   return 0;
 }
