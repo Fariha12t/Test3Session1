70c18baf644463bbd95c0ad7cfa12e33c9a9ce7f
Fariha12t <96039101+Fariha12t@users.noreply.github.com>
2022-03-31 10:59:10 +0000

    ncr

--- p3original.c
+++ p3original.c
@@ -12,19 +12,19 @@ int ncr(int n, int r)
   ncr=fact(n)/(fact(r)*fact(n-r));
 }
 int fact(int a)
-  {
-    if(a==1)
-      return 1;
-    else 
-      return a*fact(a-1);
-  }
+{
+  if(a==1)
+    return 1;
+  else 
+    return a*fact(a-1);
+}
 void output(int n, int r , int result)
 {
   printf("nCr factorial of %d and %d is %d", n,r,result);
 }
 int main()
 {
-  int n, r,result;
+  int n,r,result;
   input(&n,&r);
   result=ncr(n,r);
   output(n,r,result);
